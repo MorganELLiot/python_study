80d82279f9668f5ccf126ac46e329eb54d71cf86
Morgan Elliot <morganelliot@Morgans-MacBook-Pro.local>
2022-09-23 10:25:15 -0400

    Make a few files to get this repo set up

---
+++ full_name.py
@@ -0,0 +1,6 @@
+first_name = "morgan"
+last_name = "elliot"
+chinese_name = "雷沐淵"
+full_name = f"{first_name} {last_name} {chinese_name}"
+intro = f"Hello, my name is {full_name.title()}"
+print(intro)
---
+++ name.py
@@ -0,0 +1,4 @@
+name = "moRGan elLiOt 雷沐淵"
+print(name.title())
+print(name.upper())
+print(name.lower())
