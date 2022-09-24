6eeeda412744948dab0612a2f2c1cba1d715d693
Morgan Elliot <morganelliot@Morgans-MacBook-Pro.local>
2022-09-23 22:24:56 -0400

    Two more practice files

---
+++ apostrophe.py
@@ -0,0 +1,2 @@
+message = "One of python's strength's is that it's got a diverse community."
+print(message)
---
+++ whitespace.py
@@ -0,0 +1,5 @@
+print("Python")
+print("\tPython")
+print("Languages I like:\nPython\nJavascript\n\tTypescript")
+favorite_language = " Python      "
+print(favorite_language.strip())
